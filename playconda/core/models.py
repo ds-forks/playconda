6bcb9506e7de5458ccb3695168215ce4e035b5f0
dhilipsiva <dhilipsiva@gmail.com>
2016-04-30 10:20:54 +0530

    Adding models

--- playconda/core/models.py
+++ playconda/core/models.py
@@ -1,5 +1,29 @@
 from __future__ import unicode_literals
 
-from django.db import models
+from django.db.models import Model, CharField, ManyToManyField, EmailField, \
+    URLField
 
-# Create your models here.
+
+class Term(Model):
+    text = CharField(max_length=64, unique=True)
+
+    def __str__(self):
+        return self.term
+
+    def __repr__(self):
+        return "<Term: %s>" % self.__str__()
+
+
+class App(Model):
+    terms = ManyToManyField(Term, related_name="apps")
+    app_id = CharField(max_length=64, unique=True)
+    app_name = CharField(max_length=256)
+    developer_name = CharField(max_length=128)
+    developer_email = EmailField(max_length=128)
+    icon_url = URLField(max_length=256)
+
+    def __str__(self):
+        return self.app_id
+
+    def __repr__(self):
+        return "<App: %s>" % self.__str__()
